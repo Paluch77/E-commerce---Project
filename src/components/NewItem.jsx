369c8f2099f6b8fc96e5fbf6ca0449970d77769b
Paluch77 <58185659+Paluch77@users.noreply.github.com>
2022-10-06 10:39:42 +0200

    .

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -5,31 +5,46 @@ import xLogo from "../logos/x.png";
 import rotate from "../logos/rotate.png";
 
 function NewItem() {
-  const [file, setFile] = useState("");
-
+  const [file, setFile] = useState([]);
+  const [rotateState, setRotate] = useState("dupa");
   const inputClick = () => {
     const input = document.querySelector(".photo--input");
     input.click();
   };
 
-  const Examplefun = (x, y) => {
+  const FilesFilter = (x, y) => {
     const files = x.filter(
       (elem) =>
         elem[0].props.children[1].props.children[0].props.alt !== y.target.alt
     );
-    console.log(files);
     return files;
   };
 
   const deleteImage = (e) => {
     let fileFiltered = file.filter((elem) =>
       elem.length > 1
-        ? Examplefun(elem, e)
-        : elem[0].props.children[1].props.children[0].props.alt !== e.target.alt
+        ? FilesFilter(elem, e)
+        : elem.props.children[1].props.children[0].props.alt !== e.target.alt
     );
 
     setFile(fileFiltered);
   };
+
+  const rotateImage = (e) => {
+    let filesAll = [];
+    file.forEach((elem) => {
+      if (elem.props.children[1].props.children[0].props.alt === e.target.alt) {
+        console.log("jesttu");
+        setRotate("rotateimg90");
+        filesAll.push(elem);
+      } else {
+        filesAll.push(elem);
+      }
+    });
+    console.log(filesAll);
+    setFile(filesAll);
+  };
+
   const handleImage = (e) => {
     const files = e.target.files;
     let wholeFiles = [];
@@ -38,7 +53,7 @@ function NewItem() {
       let imageDiv = (
         <div className="photo--div" key={file.name}>
           <img
-            className="input--photo"
+            className={"input--photo " + file.name + rotateState}
             src={URL.createObjectURL(file)}
             alt={file.name}
           ></img>
@@ -48,29 +63,59 @@ function NewItem() {
               src={xLogo}
               alt={file.name}
             ></img>
-            <img className="rotate-logo" src={rotate} alt="rotate"></img>
+            <img className="rotate-logo" src={rotate} alt={file.name}></img>
           </div>
         </div>
       );
       wholeFiles.push(imageDiv);
     });
-    console.log(wholeFiles);
+
     setFile((oldArray) =>
-      oldArray.length > 0 ? [...oldArray, wholeFiles] : [wholeFiles]
+      Object.values(oldArray).length > 0 &&
+      Object.values(oldArray).length + wholeFiles.length <= 5
+        ? [...oldArray, ...wholeFiles]
+        : [
+            ...oldArray,
+            ...wholeFiles.slice(0, 5 - Object.values(oldArray).length),
+          ]
     );
   };
 
   useEffect(() => {
     let docu = document.querySelectorAll(".x-logo");
+    let rotateButton = document.querySelectorAll(".rotate-logo");
+    let photoButton = document.querySelector(".photo--button");
+    let photoButtonPlus = document.querySelector(".photo--button--plus");
+
+    if (file.length > 0) {
+      photoButton.style.display = "none";
+      photoButtonPlus.style.display = "block";
+    } else {
+      photoButton.style.display = "block";
+      photoButtonPlus.style.display = "none";
+    }
+
+    if (file.length >= 5) {
+      photoButton.style.display = "none";
+      photoButtonPlus.style.display = "none";
+    }
     if (docu) {
       docu.forEach((elem) => {
         elem.addEventListener("click", (e) => deleteImage(e));
       });
-    } else console.log(docu);
-    return () =>
+      rotateButton.forEach((elem) => {
+        elem.addEventListener("click", (e) => rotateImage(e));
+      });
+    }
+
+    return () => {
       docu.forEach((elem) =>
         elem.removeEventListener("click", (e) => deleteImage(e))
       );
+      rotateButton.forEach((elem) => {
+        elem.addEventListener("click", (e) => rotateImage(e));
+      });
+    };
   }, [file]);
 
   return (
@@ -89,6 +134,7 @@ function NewItem() {
                 <span className="photo--button" onClick={inputClick}>
                   + Dodaj Zdjęcie
                 </span>
+
                 <input
                   type="file"
                   className="photo--input"
@@ -96,6 +142,9 @@ function NewItem() {
                   onChange={handleImage}
                 ></input>
                 {file}
+                <span className="photo--button--plus" onClick={inputClick}>
+                  +
+                </span>
                 {/* <div className="photo--div">
                   <img className="input--photo" src={file} alt={file}></img>
                   <img className="x-logo" src={xLogo}></img>
